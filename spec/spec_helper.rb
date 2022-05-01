9219224389820a4d67964654c43d14cdd5ff7a52
kyriri <git-kyriri@opayq.com>
2022-05-01 23:36:25 +0200

    Config rspec --next-failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  # allow use of $ rspec --next-failure
+  config.example_status_persistence_file_path = "rspec_results.txt"
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
