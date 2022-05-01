9ee08039ae669d6ed1a308860cd1967d02cbe753
kyriri <git-kyriri@opayq.com>
2022-05-02 00:43:48 +0200

    Add list of warehouses to homepage

--- spec/system/user_views_homepage_spec.rb
+++ spec/system/user_views_homepage_spec.rb
@@ -3,7 +3,27 @@ require 'rails_helper'
 describe 'User visits homepage' do
   it 'and see app name' do
     visit('/')
+
     expect(page).to have_content('Galpões & Estoque')
+  end
+
+  it 'and see warehouses' do
+    Warehouse.create!(name: 'Belém', code: 'BEL', city: 'Ananindeua', area: 10_000)
+    Warehouse.create!(name: 'São Paulo', code: 'SAO', city: 'Osasco', area: 50_000)
+
+    visit('/')
+
+    expect(page).not_to have_content('Nenhum galpão cadastrado.')
+    expect(page).to have_content('Belém')
+    expect(page).to have_content('SAO')
+    expect(page).to have_content('Ananindeua')
+    expect(page).to have_content('50000 m²')
+  end
+
+  it 'and there are no warehouses to display' do
+    visit('/')
+
+    expect(page).to have_content('Nenhum galpão cadastrado.')
   end
 end
 
