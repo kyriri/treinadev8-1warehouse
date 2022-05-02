8114944867f919d06e2337c0822cd7e2d73b5555
kyriri <git-kyriri@opayq.com>
2022-05-03 01:22:08 +0200

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Warehouse.destroy_all
+
+Warehouse.create!([{name: 'Belém', 
+                    code: 'BEL', 
+                    area: 10_000,
+                    directions: "Caminho do Mar, 950 - perto do posto Piranga'y",
+                    city: 'Ananindeua',
+                    cep: '67140-000',
+                    description: 'Handles deliveries for PA and MA.',
+                  }, {
+                    name: 'São Paulo', 
+                    code: 'SAO', 
+                    area: 50_000,
+                    directions: "Rua dos Automobilistas, s/n",
+                    city: 'Osasco', 
+                    cep: '0615-040',
+                    description: 'Main packaging facility, and deliveries to West of São Paulo.',
+                  }])
+
+p "Created #{Warehouse.count} warehouse(s)"
