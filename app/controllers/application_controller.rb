e2bd9a79ac6b0af59e8f51dc6360bb8c7841cb2e
kyriri <git-kyriri@opayq.com>
2022-05-02 15:14:26 +0200

    Set up i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+def switch_locale(&action)
+  locale = params[:locale] || I18n.default_locale
+  # locale = current_user.try(:locale) || I18n.default_locale # persists choice for logged users
+  I18n.with_locale(locale, &action)
+end
+
 end
