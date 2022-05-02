24a94c155593457fbc67f86e39c00ee60f2eb368
kyriri <git-kyriri@opayq.com>
2022-05-03 01:11:43 +0200

    Add warehouse details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+  
+  resources :warehouses, only: [:show]
 end
---
+++ spec/system/user_views_warehouse_details_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'User visits warehouse details page' do
+  it 'succesfully' do
+    pa = Warehouse.create!(name: 'Belém', code: 'BEL', area: 10_000,
+                          directions: "Caminho do Mar, 950 - perto do posto Piranga'y",
+                          city: 'Ananindeua',
+                          cep: '67140-000',
+                          description: 'Handles deliveries for PA and MA.')
+    
+    visit root_path
+    click_on 'Belém'
+
+    expect(page).to have_content('Belém')
+    expect(page).to have_content('BEL')
+    expect(page).to have_content('10000 m²')
+    expect(page).to have_content("Caminho do Mar, 950 - perto do posto Piranga'y")
+    expect(page).to have_content('Ananindeua')
+    expect(page).to have_content('67140-000')
+    expect(page).to have_content('Handles deliveries for PA and MA.')
+  end
+end
