812fdebf2516dfddc0725d84862a60e8f34f4109
kyriri <git-kyriri@opayq.com>
2022-05-03 17:07:40 +0200

    Add "button" back to homepage on warehouse page

--- spec/system/user_views_warehouse_details_spec.rb
+++ spec/system/user_views_warehouse_details_spec.rb
@@ -19,4 +19,17 @@ describe 'User visits warehouse details page' do
     expect(page).to have_content('67140-000')
     expect(page).to have_content('Handles deliveries for PA and MA.')
   end
+
+  it 'and goes back to homepage' do
+    pa = Warehouse.create!(name: 'Belém', code: 'BEL', area: 10_000,
+                          directions: "Caminho do Mar, 950 - perto do posto Piranga'y",
+                          city: 'Ananindeua',
+                          cep: '67140-000',
+                          description: 'Handles deliveries for PA and MA.')
+
+    visit warehouse_path(pa.id)
+    click_on 'Voltar'
+    
+    expect(current_path).to eq('/')
+  end
 end
