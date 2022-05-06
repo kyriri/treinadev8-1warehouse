b5be7f4c41cae930656b6fd1ff8244dd29fe2f98
kyriri <git-kyriri@opayq.com>
2022-05-06 20:03:44 +0200

    Test presence of all fields for model Warehouse

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe '#valid?' do
+    context '(presence validation)' do
+      it 'is true when all fields are filled' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq true
+      end
+
+      it 'is false when name is blank' do
+        warehouse = Warehouse.create(name: '', code: 'CHF', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when code is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: '', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when area is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: '',
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when directions is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                                    directions: '',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when city is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: '', state: 'SC',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when state is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: '',
+                                    cep: '15520-000',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when cep is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '',
+                                    description: 'Handles all chocolate products, including Easter eggs')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+
+      it 'is false when description is blank' do
+        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                                    directions: 'Av. Rocher, s/n',
+                                    city: 'Chocolândia', state: 'SC',
+                                    cep: '15520-000',
+                                    description: '')
+        
+        response = warehouse.valid?
+
+        expect(response).to eq false
+      end
+    end
+  end
 end
