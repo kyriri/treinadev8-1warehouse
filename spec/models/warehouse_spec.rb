68d3c814abc66052ce83bc6c9507054ec8496fcb
kyriri <git-kyriri@opayq.com>
2022-05-07 15:26:18 +0200

    Test uniqueness of "code" field for model Warehouse

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Warehouse, type: :model do
   describe '#valid?' do
     context '(presence validation)' do
       it 'is true when all fields are filled' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '15520-000',
@@ -16,7 +16,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when name is blank' do
-        warehouse = Warehouse.create(name: '', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: '', code: 'CHF', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '15520-000',
@@ -28,7 +28,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when code is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: '', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: '', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '15520-000',
@@ -40,7 +40,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when area is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: '',
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: '',
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '15520-000',
@@ -52,7 +52,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when directions is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
                                     directions: '',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '15520-000',
@@ -64,7 +64,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when city is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: '', state: 'SC',
                                     cep: '15520-000',
@@ -76,7 +76,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when state is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: '',
                                     cep: '15520-000',
@@ -88,7 +88,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when cep is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '',
@@ -100,7 +100,7 @@ RSpec.describe Warehouse, type: :model do
       end
 
       it 'is false when description is blank' do
-        warehouse = Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+        warehouse = Warehouse.new(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
                                     directions: 'Av. Rocher, s/n',
                                     city: 'Chocolândia', state: 'SC',
                                     cep: '15520-000',
@@ -109,6 +109,23 @@ RSpec.describe Warehouse, type: :model do
         response = warehouse.valid?
 
         expect(response).to eq false
+      end
+    end
+
+    context '(uniqueness validation)' do
+      it 'is false when code was already in use' do
+        Warehouse.create(name: 'Oompa Loompa', code: 'CHF', area: 1_000,
+                        directions: 'Av. Rocher, s/n',
+                        city: 'Chocolândia', state: 'SC',
+                        cep: '15520-000',
+                        description: 'Handles all chocolate products, including Easter eggs')
+        warehouse = Warehouse.new(name: 'Ninho', code: 'CHF', area: 5_000,
+                                  directions: 'Via Laktea, 35',
+                                  city: 'Cacaulândia', state: 'PR',
+                                  cep: '25045-989',
+                                  description: 'Packages chocolate products.')
+          
+        expect(warehouse).not_to be_valid
       end
     end
   end
