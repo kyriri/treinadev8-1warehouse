965e2629c3c054efa609bf9138e671ffeec32bd0
kyriri <git-kyriri@opayq.com>
2022-05-08 15:05:45 +0200

    Display error msgs on New Warehouse form

--- spec/system/user_registers_warehouse_spec.rb
+++ spec/system/user_registers_warehouse_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User tries to register a new warehouse' do
-  it 'succesfully' do
+  it 'and is succesful' do
 
   visit root_path
   click_on 'Cadastrar novo galpão'
@@ -26,10 +26,26 @@ describe 'User tries to register a new warehouse' do
     click_on 'Registrar Galpão'
 
     expect(page).to have_text('Houve um problema. O galpão não foi salvo.')
+    expect(page).not_to have_text('salvo com sucesso')
+    expect(page).to have_text('Nome não pode ficar em branco').once
+    expect(page).to have_text('não pode ficar em branco').exactly(8)
   end
 
-  xit "but code doesn't have 3 chars" do
-    # Constrain Warehouse codes to 3 chars
-    # validates :registration_number, length: { is: 6 }
+  it "but they put a CEP in a wrong format" do
+
+    visit new_warehouse_path
+    fill_in 'Nome', with: 'Belém'
+    fill_in 'Código', with: 'BEL'
+    fill_in 'Área', with: 10_000
+    fill_in 'Linha 1', with: "Caminho do Mar, 950 - perto do posto Piranga'y"
+    fill_in 'Cidade', with: 'Ananindeua'
+    fill_in 'Estado', with: 'PA'
+    fill_in 'CEP', with: '6714-0000'
+    fill_in 'Descrição', with: 'Handles deliveries for PA and MA.'
+    click_on 'Registrar Galpão'
+
+    expect(page).to have_text('Houve um problema. O galpão não foi salvo.')
+    expect(page).not_to have_text('salvo com sucesso')
+    expect(page).to have_text('CEP deve ter formato 00000-000').once
   end
 end
