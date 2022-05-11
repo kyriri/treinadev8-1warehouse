0156377dc4f43a2cd032f55bb6214b092de64a9b
kyriri <git-kyriri@opayq.com>
2022-05-11 04:31:26 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Warehouse.destroy_all
+Manufacturer.destroy_all
 
 Warehouse.create!([{name: 'Belém', 
                     code: 'BEL', 
@@ -27,4 +28,20 @@ Warehouse.create!([{name: 'Belém',
                     description: 'Main packaging facility, and deliveries to West of São Paulo.',
                   }])
 
+Manufacturer.create!([{tradename: 'Pear',
+                      registered_name: 'Pear do Brasil LTDA',
+                      address: 'Av. Cooper Tinu, 3 - Manaus, AM',
+                      email: 'comercial@pear.com.br',
+                      telephone: '(92) 3026-8573',
+                      cnpj: 1234567890123,
+                    }, {
+                      tradename: 'Pomme de terre',
+                      registered_name: 'Patatis Brasilis Ltda',
+                      address: 'Rodovia PR 354 km 12, Fritol',
+                      email: 'contato@pdt.com.br',
+                      telephone: '(45) 9 9656-0932',
+                      cnpj: 1234567890000,
+                    }])
+
 p "Created #{Warehouse.count} warehouse(s)"
+p "Created #{Manufacturer.count} manufacturer(s)"
